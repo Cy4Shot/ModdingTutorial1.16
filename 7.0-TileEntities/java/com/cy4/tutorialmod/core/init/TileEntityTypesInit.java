9ed4f56a5419705dfaf7f95a189399a8a278e2bf
Cy4Shot <veschi@gmail.com>
2021-02-16 20:05:52 +0000

    7.0

---
+++ 7.0-TileEntities/java/com/cy4/tutorialmod/core/init/TileEntityTypesInit.java
@@ -0,0 +1,20 @@
+package com.cy4.tutorialmod.core.init;
+
+import com.cy4.tutorialmod.TutorialMod;
+import com.cy4.tutorialmod.common.te.QuarryTileEntity;
+
+import net.minecraft.tileentity.TileEntityType;
+import net.minecraftforge.fml.RegistryObject;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
+public class TileEntityTypesInit {
+
+	public static final DeferredRegister<TileEntityType<?>> TILE_ENTITY_TYPE = DeferredRegister
+			.create(ForgeRegistries.TILE_ENTITIES, TutorialMod.MOD_ID);
+
+	public static final RegistryObject<TileEntityType<QuarryTileEntity>> QUARRY_TILE_ENTITY_TYPE = TILE_ENTITY_TYPE
+			.register("quarry",
+					() -> TileEntityType.Builder.create(QuarryTileEntity::new, BlockInit.QUARRY.get()).build(null));
+
+}
