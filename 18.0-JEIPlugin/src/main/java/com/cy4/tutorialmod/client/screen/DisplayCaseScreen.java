e569a7390c556a61267617f342b4809c7df6713e
Cy4Shot <veschi@gmail.com>
2021-05-16 12:40:09 +0100

    Episode 18.0

---
+++ 18.0-JEIPlugin/src/main/java/com/cy4/tutorialmod/client/screen/DisplayCaseScreen.java
@@ -0,0 +1,54 @@
+package com.cy4.tutorialmod.client.screen;
+
+import com.cy4.tutorialmod.TutorialMod;
+import com.cy4.tutorialmod.common.container.DisplayCaseContainer;
+import com.mojang.blaze3d.matrix.MatrixStack;
+import com.mojang.blaze3d.systems.RenderSystem;
+
+import net.minecraft.client.gui.screen.inventory.ContainerScreen;
+import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+
+@OnlyIn(Dist.CLIENT)
+public class DisplayCaseScreen extends ContainerScreen<DisplayCaseContainer> {
+
+	private static final ResourceLocation DISPLAY_CASE_GUI = new ResourceLocation(TutorialMod.MOD_ID,
+			"textures/gui/display_case.png");
+
+	public DisplayCaseScreen(DisplayCaseContainer screenContainer, PlayerInventory inv, ITextComponent titleIn) {
+		super(screenContainer, inv, titleIn);
+
+		this.guiLeft = 0;
+		this.guiTop = 0;
+		this.xSize = 175;
+		this.ySize = 201;
+	}
+
+	@Override
+	public void render(MatrixStack matrixStack, int mouseX, int mouseY, float partialTicks) {
+		this.renderBackground(matrixStack);
+		super.render(matrixStack, mouseX, mouseY, partialTicks);
+		this.renderHoveredTooltip(matrixStack, mouseX, mouseY);
+	}
+
+	@Override
+	protected void drawGuiContainerForegroundLayer(MatrixStack matrixStack, int x, int y) {
+		this.font.func_243248_b(matrixStack, this.playerInventory.getDisplayName(), (float) this.playerInventoryTitleX,
+				(float) this.playerInventoryTitleY, 4210752);
+	}
+
+	@SuppressWarnings("deprecation")
+	@Override
+	protected void drawGuiContainerBackgroundLayer(MatrixStack matrixStack, float partialTicks, int mouseX,
+			int mouseY) {
+		RenderSystem.color4f(1f, 1f, 1f, 1f);
+		this.minecraft.textureManager.bindTexture(DISPLAY_CASE_GUI);
+		int x = (this.width - this.xSize) / 2;
+		int y = (this.height - this.ySize) / 2;
+		this.blit(matrixStack, x, y, 0, 0, this.xSize, this.ySize);
+	}
+
+}
